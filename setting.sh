efd89ea180cbf3271300c3b7f581f8f22a76ac9e
kissesy <b7712983@naver.com>
2019-07-04 18:38:05 +0900

    setting

---
+++ setting.sh
@@ -0,0 +1,15 @@
+
+sudo apt update -y
+sudo apt upgrade -y
+sudo apt install gnome-tweaks -y
+sudo apt install git -y
+sudo apt install vim -y 
+sudo apt install pinta 
+sudo apt install tmux -y 
+wget https://raw.githubusercontent.com/scwuaptx/vimrc/master/install.sh
+sudo apt update -y 
+sudo apt install gnome-shell-extensions
+sudo apt-get install albert
+sudo apt install tlp 
+firefox www.gnome-look.org 
+
